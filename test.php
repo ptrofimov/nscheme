a4f3bbd533c46e86d0f737f4b7b1faf0c4694ae5
ptrofimov <ptrofimov@rbc.ru>
2012-08-03 14:42:17 +0400

    added hash

--- test.php
+++ test.php
@@ -9,15 +9,21 @@ class MyScheme extends NScheme
 		parent::__construct( SERVER );
 		$this->_define( 'some_value' );
 		$this->_define( 'some_set', 'set' );
-		//$this->_define( 'some_hash', 'hash' );
+		$this->_define( 'some_hash', 'hash' );
 		//$this->_define( 'some_queue', 'queue' );
-		//$this->_define( 'some_queue', 'queue' );
+		//$this->_define( 'some_stack', 'stack' );
 	}
 }
 
 $my = new MyScheme();
+
 var_dump( $my->some_value = 'value' );
 var_dump( $my->some_value );
+
 var_dump( $my->some_set->add( 'value' ) );
 var_dump( $my->some_set->exists( 'value' ) );
 var_dump( $my->some_set->get() );
+
+var_dump( $my->some_hash[ 'key' ] = 'value' );
+var_dump( isset( $my->some_hash[ 'key' ] ) );
+var_dump( $my->some_hash[ 'key' ] );
