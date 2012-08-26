05da8121a3d436ca0f3132f020ff69549ce498f1
ptrofimov <D:\temp\batyandex>
2012-08-26 22:38:59 +0400

    added case for value

--- tests/Suite.test.php
+++ tests/Suite.test.php
@@ -2,23 +2,27 @@
 /**
  * @author Petr Trofimov
  */
-require_once 'NScheme.test.php';
+require_once ( dirname( __FILE__ ) . '/../NScheme/NScheme.class.php' );
+require_once ( 'include/TinyRedisClient.class.php' );
+require_once ( 'include/MyScheme.class.php' );
+require_once 'Value.test.php';
+
 class MySuite extends PHPUnit_Framework_TestSuite
 {
 	public static function suite()
 	{
 		$suite = new MySuite();
-		$suite->addTestSuite( 'NSchemeTest' );
+		$suite->addTestSuite( 'ValueTest' );
 		return $suite;
 	}
 	
 	protected function setUp()
 	{
-		print "\nMySuite::setUp()";
+		//print "\nMySuite::setUp()";
 	}
 	
 	protected function tearDown()
 	{
-		print "\nMySuite::tearDown()";
+		//print "\nMySuite::tearDown()";
 	}
 }
