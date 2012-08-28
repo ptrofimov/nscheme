4449a243d1127c208dfc41738bd194f89415f993
ptrofimov <D:\temp\batyandex>
2012-08-28 23:26:07 +0400

    updated config.php, tests FAILED

--- tests/include/config.php
+++ tests/include/config.php
@@ -2,9 +2,14 @@
 require_once ( dirname( __FILE__ ) . '/../../NScheme/NScheme.class.php' );
 require_once ( 'clients/tinyredisclient/TinyRedisClient.class.php' );
 require_once ( 'clients/rediska/Rediska.php' );
+require_once ( 'clients/memcached/Memcached.php' );
 require_once ( 'TestScheme.class.php' );
 
+$memcached = new Memcached();
+$memcached->addServer('localhost', 11211);
+
 $clients = array( 
-	new TinyRedisClient( 'localhost:6379' ), 
-	new Rediska( array( 'servers' => array( array( 'host' => 'localhost', 'port' => 6379 ) ) ) )
+	//new TinyRedisClient( 'localhost:6379' ), 
+	//new Rediska( array( 'servers' => array( array( 'host' => 'localhost', 'port' => 6379 ) ) ) ),
+	$memcached,
 		 );
