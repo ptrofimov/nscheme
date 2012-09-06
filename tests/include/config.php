e7c9f3e9145334d04a51114188e5001b841d4560
ptrofimov <ptrofimov@rbc.ru>
2012-09-06 14:39:14 +0400

    added litememcache to tests

--- tests/include/config.php
+++ tests/include/config.php
@@ -2,14 +2,11 @@
 require_once ( dirname( __FILE__ ) . '/../../NScheme/NScheme.class.php' );
 require_once ( 'clients/tinyredisclient/TinyRedisClient.class.php' );
 require_once ( 'clients/rediska/Rediska.php' );
-require_once ( 'clients/memcached/Memcached.php' );
+require_once ( 'clients/litememcache/LiteMemcache.class.php' );
 require_once ( 'TestScheme.class.php' );
-
-$memcached = new Memcached();
-$memcached->addServer('localhost', 11211);
 
 $clients = array( 
 	//new TinyRedisClient( 'localhost:6379' ), 
 	//new Rediska( array( 'servers' => array( array( 'host' => 'localhost', 'port' => 6379 ) ) ) ),
-	$memcached,
+	new LiteMemcache( 'localhost:11211' ), 
 		 );
